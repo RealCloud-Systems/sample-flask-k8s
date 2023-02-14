ae320129e5b099d373df772be985ba509b6b73c2
marcosportorealcloud <marcos.porto@realcloud.com.br>
2023-02-14 09:37:04 -0300

    Adding src

---
+++ src/app.py
@@ -0,0 +1,28 @@
+import psutil
+import socket
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+
+def get_details():
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    return str(host_name), str(host_ip)
+
+@app.route("/cpu")
+def get_cpu_percentage():
+    cpu_percent = psutil.cpu_percent()
+    return jsonify({"cpu_percent": cpu_percent})
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World</p>"
+
+@app.route("/details")
+def detail():
+    hostname, ip = get_details()
+    return render_template("index.html", HOSTNAME=hostname, IP=ip)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
